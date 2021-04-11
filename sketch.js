3c97e4cb449fcec2c3db263b3b223179fb0261dd
arti-garg <75868830+arti-garg@users.noreply.github.com>
2021-04-11 22:01:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+  //level one
+  block1 = new Block(300,275,30,40);
+  //ball holder with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:200});
+
+}
+function draw() {
+  background(56,44,44); 
+ 
+  //Engine.update(engine);
+  ground.display();
+  stand1.display();
+  stand2.display();
+  strokeWeight(2);
+  stroke(15);
+  fill("skyblue");
+  block1.display();
+
+
+  imageMode(CENTER)
+  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
